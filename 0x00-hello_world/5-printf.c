33218b6079e6fd66103c6116a0f19e300ae28292
Baktu <bekitasweet.2013@gmail.com>
2022-03-14 14:47:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main -printf point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome isa piece of art,");
+return (0);
+}
