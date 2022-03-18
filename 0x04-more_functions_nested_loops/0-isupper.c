6441063076fb660c1f5792fafdb09171c921f861
Bektu <bekitasweet.2013@gmail.com>
2022-03-19 01:01:26 +0300

    add

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
  * _isuper - Checks if c is uppercase character
- * @c : Character to be checked
+ * @c: Character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c);{
-	if(c>="A" && c<="Z")
+int _isupper(int c)
+{
+	if(c >= "A" && c <= "Z")
 		return(1);
 	else
 		return(0);
