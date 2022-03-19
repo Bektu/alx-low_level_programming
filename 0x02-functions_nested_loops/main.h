33b233ee0227ae9cf30209bb1a940e347eb0bc76
Baktu <bekitasweet.2013@gmail.com>
2022-03-19 11:39:50 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
@@ -9,7 +9,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_tale(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+
 #endif
