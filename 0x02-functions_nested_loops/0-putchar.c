a0c45dd2b97ed11e01c5c2dde3ed05bb47b16d63
Baktu <bekitasweet.2013@gmail.com>
2022-03-19 11:02:49 -0700

    function commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
 * main - Entry point
-* @void: no argument
-* Return: 0
-**/
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char pputchar[] = "_putchar";
-int c
-for(c=0; c< 8; c++)
-{
-_putchar(pputchar[]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
