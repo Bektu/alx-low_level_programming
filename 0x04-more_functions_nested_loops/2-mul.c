8304e1e9d2216fda576ee19096b22e8019dca53b
Baktu <bekitasweet.2013@gmail.com>
2022-03-21 15:15:16 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiplies two integers
+* @a: print int
+* @b: print int
+* Return: Always 0.
+*/
+int mup(int a, int b)
+{
+return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include<stdio.h>
+int mul_int a, int b);
 int _isupper(int c);
 int _isdigit(int c):
 int _putchar(char c);
