190c7bc54b3c440da7138afbd18a16f0ae828e13
Baktu <bekitasweet.2013@gmail.com>
2022-03-21 13:48:47 -0700

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int _isupper(int c);
-if(c>="A" && c<="Z")
-return(1);
+* _isdigit - checks whether it is number or not
+* main - check the code.
+*@c: print int
+* Return: Always 0.
+*/
+int _isdigit(int c);
+if (c >= "48" && c <= "57")
+return (1);
 else
-return(0);
+return (0);
 
