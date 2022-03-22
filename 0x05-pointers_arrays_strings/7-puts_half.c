d6ec575e4fe092df2e801223763808673ea073ff
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 15:48:59 -0700

    new commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,12 +8,12 @@ void puts_half(char *str)
 int i = 0;
 int n;
 while (str[i] != '\0')
-}
-i++
+{
+i++;
 }
 if (i % 2 == 0)
 {
-for (n = i/2; str[n] != 0; n++)
+for (n = i / 2; str[n] != 0; n++)
 {
 _putchar (str[n]);
 }
@@ -22,7 +22,7 @@ else
 {
 for (n = (i - 1) / 2; n< i - 1; n++)
 {
-_putcharstr[n + 1]);
+  _putchar (str[n + 1]);
 }
 }
 _putchar('\n');
