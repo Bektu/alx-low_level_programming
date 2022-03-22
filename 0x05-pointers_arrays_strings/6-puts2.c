a6b179385ea0ad52af22c7b47fbf32358985842e
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 15:37:25 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* puts2 - function that prints every other character of a string
+*@str: array.
+*/
+void puts2(char *str)
+{
+int len = 0;
+int z;
+while (str[len] != 0)
+{
+len++;
+}
+for (z = 0; z < len; z++)
+{
+if (z % 2) == 0)
+{
+_putchar(str[z]);
+}
+}
+_putchar('\n');
+}
