3c6fc677e2f0a254ffed5e57aa4ed49d922c9ab4
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 15:38:58 -0700

    new commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _strcpy - copies the string pointed to by src, including terminatingnull byte (\0), to the buffer pointed to by dest
+* _strcpy - copies the string pointed to by src, including terminatingnull
+* byte (\0), to the buffer pointed to by dest
 *@src: string
 *@dest: buffer pointer
 * Return: void
