c5ef417f2606594b8759588a7d9c84a39f8716ed
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 14:24:11 -0700

    new commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
 * swap_int - a function that swaps the values
-* @a: integer number 1;
+*@a: integer number 1;
 * @b: integer number 2;
 * Return: Always 0.
+*/
 void swap_int(int *a, int *b)
 {
 int c;
