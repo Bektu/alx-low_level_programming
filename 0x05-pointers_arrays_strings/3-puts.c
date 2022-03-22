a22879776ba30f7cd792f6cda0909ec8ef710fcb
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 14:51:40 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string
+ * @str:
+ * Return: always 0.
+ */
+void _puts(char *str)
+{
+char *str;
+str = "I do not fear computers. I fear the lackof thm - Isaac Asi,ov";
+puts(str);
+return (0);
+}
