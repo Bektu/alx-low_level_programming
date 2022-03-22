06b98c988baa22a7284004932f82d7b2b534794e
Baktu <bekitasweet.2013@gmail.com>
2022-03-22 14:36:18 -0700

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _strlen - a functiomn that gives the length of tring
+*@s: string 1
+* Return: always 0.
+*/
+int _strlen(char *s)
+{
+s = "My first strlen!";
+len = _strlen(*s);
+return (0);
+}
