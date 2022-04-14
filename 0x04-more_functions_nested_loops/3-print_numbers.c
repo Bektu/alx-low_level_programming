4945b9ba9e9ba128bbfeda4f801d237b6917005f
Baktu <bekitasweet.2013@gmail.com>
2022-04-14 04:25:15 -0700

    more loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers -prints given numbers.
+* Return: always 0.
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+putchar('\n');
+}
+return (0);
+}
