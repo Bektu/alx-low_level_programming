bcb93b46b9fd72da4a1465ca3ab522a3a4a58c3a
Bektu <bekitasweet.2013@gmail.com>
2022-06-16 12:04:38 +0300

    rec

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* _puts_recursion - prints a string followed by a new line
+* _puts_recursion - prints a string
 * @s: string to print
-*
 * Return: 0.
 */
 void _puts_recursion(char *s)
