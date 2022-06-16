969043d8e30f872b8f716fb4d59748791fccfd5e
Bektu <bekitasweet.2013@gmail.com>
2022-06-16 22:12:33 +0300

    git

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,54 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _isupper(int c);
+int _isupper(int c#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int number, tens, units, row;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+	}
+}#include "main.h"
+
+/**
+ * more_numbers - nmbr fct
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int number, tens, units, row;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (number = 0; number <= 14; number++)
+		{
+			tens = number / 10;
+			units = number % 10;
+			if (number > 9)
+				_putchar(tens + '0');
+
+			_putchar(units + '0');
+		}
+		_putchar('\n');
+	}
+});
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
